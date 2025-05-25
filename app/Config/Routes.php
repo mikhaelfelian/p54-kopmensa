5e567f2fae363b0877d864d8206c2f1a78d1a686
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-05-26 00:46:34 +0700

    Initial commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,4 +5,15 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+$routes->get('/', to: 'Home::index');
+
+// Auth routes
+$routes->group('auth', function ($routes) {
+    $routes->get('/', 'Auth::index');
+    $routes->match(['get', 'post'], 'login', 'Auth::login');
+    $routes->post('cek_login', 'Auth::cek_login');
+    $routes->get('logout', 'Auth::logout');
+    $routes->match(['get', 'post'], 'forgot_password', 'Auth::forgot_password');
+});
+
+$routes->get('/dashboard', 'Dashboard::index', ['namespace' => 'App\Controllers', 'filter' => 'auth']);
