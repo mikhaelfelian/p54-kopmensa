d2ec2e29c01967abaaa7480bf27b2b666d65c6b5
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-05-26 00:11:15 +0700

    Initial commit

--- app/Config/Autoload.php
+++ app/Config/Autoload.php
@@ -17,6 +17,8 @@ use CodeIgniter\Config\AutoloadConfig;
  *
  * NOTE: This class is required prior to Autoloader instantiation,
  *       and does not extend BaseConfig.
+ *
+ * @immutable
  */
 class Autoload extends AutoloadConfig
 {
@@ -38,7 +40,9 @@ class Autoload extends AutoloadConfig
      * @var array<string, list<string>|string>
      */
     public $psr4 = [
-        APP_NAMESPACE => APPPATH,
+        APP_NAMESPACE => APPPATH, // For custom app namespace
+        'Config'      => APPPATH . 'Config',
+        'IonAuth'     => ROOTPATH . 'CodeIgniter-Ion-Auth',
     ];
 
     /**
@@ -88,5 +92,15 @@ class Autoload extends AutoloadConfig
      *
      * @var list<string>
      */
-    public $helpers = [];
+    public $helpers = [
+        'form',
+        'url',
+        'html',
+        'theme',
+        'toastr',
+        'general',
+        'debug',
+        'angka',
+        'tanggalan'
+    ];
 }
