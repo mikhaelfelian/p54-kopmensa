c36f587377990a3c275e3366e51dbe7978c4adad
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-05-26 19:47:19 +0700

    Update Home.php

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,13 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('quirk/index');
+        
+        $data = [
+            'title'         => 'Dashboard',
+            'Pengaturan'    => $this->pengaturan,
+            'total_users'   => 1
+        ];
+
+        return view($this->theme->getThemePath() . '/index', $data);
     }
 }
