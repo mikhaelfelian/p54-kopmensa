c17ebb3acefa6f0a05a1dffdba6f6daa62701ecd
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-05-27 23:55:48 +0700

    Update Auth Modules
    - Refixing Auth
    - Remove score threeshold

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -9,7 +9,7 @@ $routes->get('/', to: 'Auth::login');
 
 // Auth routes
 $routes->group('auth', ['namespace' => 'App\Controllers'], function ($routes) {
-    // $routes->get('/', 'Auth::index');
+    $routes->get('/', 'Auth::index');
     $routes->get('login', 'Auth::login');
     $routes->post('cek_login', 'Auth::cek_login');
     $routes->get('logout', 'Auth::logout');
