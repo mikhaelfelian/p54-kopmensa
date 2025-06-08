58ca436d1c4a360ef8298871800d750c654db740
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-06-08 18:29:28 +0700

    Update main.php
    Modify main template

--- app/Views/quirk/layout/main.php
+++ app/Views/quirk/layout/main.php
@@ -20,29 +20,40 @@
     <link rel="stylesheet" href="<?= base_url('public/assets/theme/quirk/css/quirk.css') ?>">
 
     <script src="<?= base_url('public/assets/theme/quirk/lib/modernizr/modernizr.js') ?>"></script>
-    
-    
+
+
     <!-- Core JavaScript Libraries -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery/jquery.js') ?>"></script> <!-- jQuery core library for DOM manipulation and AJAX -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-ui/jquery-ui.js') ?>"></script> <!-- jQuery UI for enhanced user interface components -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/bootstrap/js/bootstrap.js') ?>"></script> <!-- Bootstrap JavaScript for responsive layout and components -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-toggles/toggles.js') ?>"></script> <!-- Toggle switches for UI elements -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery/jquery.js') ?>"></script>
+    <!-- jQuery core library for DOM manipulation and AJAX -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-ui/jquery-ui.js') ?>"></script>
+    <!-- jQuery UI for enhanced user interface components -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/bootstrap/js/bootstrap.js') ?>"></script>
+    <!-- Bootstrap JavaScript for responsive layout and components -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-toggles/toggles.js') ?>"></script>
+    <!-- Toggle switches for UI elements -->
 
     <!-- Chart Libraries -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/morrisjs/morris.js') ?>"></script> <!-- Morris.js for creating charts and graphs -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/raphael/raphael.js') ?>"></script> <!-- Raphael.js required by Morris.js for vector graphics -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/morrisjs/morris.js') ?>"></script>
+    <!-- Morris.js for creating charts and graphs -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/raphael/raphael.js') ?>"></script>
+    <!-- Raphael.js required by Morris.js for vector graphics -->
 
     <!-- Flot Chart Libraries -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/flot/jquery.flot.js') ?>"></script> <!-- Flot core library for plotting charts -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/flot/jquery.flot.resize.js') ?>"></script> <!-- Flot plugin for responsive chart resizing -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/flot-spline/jquery.flot.spline.js') ?>"></script> <!-- Flot plugin for smooth curve interpolation -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/flot/jquery.flot.js') ?>"></script>
+    <!-- Flot core library for plotting charts -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/flot/jquery.flot.resize.js') ?>"></script>
+    <!-- Flot plugin for responsive chart resizing -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/flot-spline/jquery.flot.spline.js') ?>"></script>
+    <!-- Flot plugin for smooth curve interpolation -->
 
     <!-- UI Components -->
-    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-knob/jquery.knob.js') ?>"></script> <!-- jQuery Knob for circular dial inputs -->
+    <script src="<?= base_url('public/assets/theme/quirk/lib/jquery-knob/jquery.knob.js') ?>"></script>
+    <!-- jQuery Knob for circular dial inputs -->
 
     <!-- Theme Specific Scripts -->
     <script src="<?= base_url('public/assets/theme/quirk/js/quirk.js') ?>"></script> <!-- Main theme JavaScript file -->
-    <script src="<?= base_url('public/assets/theme/quirk/js/dashboard.js') ?>"></script> <!-- Dashboard specific JavaScript -->
+    <script src="<?= base_url('public/assets/theme/quirk/js/dashboard.js') ?>"></script>
+    <!-- Dashboard specific JavaScript -->
 
     <!-- Toastr -->
     <link rel="stylesheet" href="<?= base_url('/public/assets/plugins/toastr/toastr.min.css') ?>">
@@ -68,7 +79,11 @@
         <!-- leftpanel -->
 
         <!-- mainpanel -->
-        <?= $this->renderSection('content') ?>
+        <div class="mainpanel">
+            <div class="contentpanel">
+                <?= $this->renderSection('content') ?>
+            </div>
+        </div>
         <!-- mainpanel -->
     </section>
 
