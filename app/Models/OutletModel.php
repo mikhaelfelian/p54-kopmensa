b04cebb8e702ea5d3fe2fa4609d08d06b003c0c7
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-06-17 22:59:32 +0700

    Create OutletModel.php
    Added Outlet Model

---
+++ app/Models/OutletModel.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+/**
+ * Created by: Mikhael Felian Waskito - mikhaelfelian@gmail.com
+ * Date: 2025-06-17
+ * Github : github.com/mikhaelfelian
+ * description : Model for managing outlet data
+ * This file represents the Model for Outlet data management.
+ */
+class OutletModel extends Model
+{
+    protected $table            = 'tbl_m_outlet';
+    protected $primaryKey       = 'id';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'array';
+    protected $useSoftDeletes   = true;
+    protected $protectFields    = true;
+    protected $allowedFields    = [
+        'id_user',
+        'kode',
+        'nama',
+        'deskripsi',
+        'status',
+        'status_hps'
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat    = 'datetime';
+    protected $createdField  = 'created_at';
+    protected $updatedField  = 'updated_at';
+    protected $deletedField  = 'deleted_at';
+
+    // Validation
+    protected $validationRules      = [];
+    protected $validationMessages   = [];
+    protected $skipValidation       = false;
+    protected $cleanValidationRules = true;
+} 
