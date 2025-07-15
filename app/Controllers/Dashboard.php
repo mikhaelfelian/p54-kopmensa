cb6868915231f5f6faac7d74f32013afbfa33a8a
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-07-15 08:31:52 +0700

    Update Dashboard.php
    Added dashboard product

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -13,15 +13,20 @@ class Dashboard extends BaseController
     protected $medTransModel;
 
     public function __construct(){
+        $this->itemModel = new \App\Models\ItemModel();
     }
     public function index()
     {        
+        // Ambil 10 produk aktif terbaru untuk dashboard
+        $items = $this->itemModel->getItemsWithRelationsActive(6);
+
         $data = [
             'title'         => 'Dashboard',
             'Pengaturan'    => $this->pengaturan,
             'user'          => $this->ionAuth->user()->row(),
             'isMenuActive'  => isMenuActive('dashboard') ? 'active' : '',
-            'total_users'   => 1
+            'total_users'   => 1,
+            'items'         => $items
         ];
 
         return view($this->theme->getThemePath() . '/dashboard', $data);
