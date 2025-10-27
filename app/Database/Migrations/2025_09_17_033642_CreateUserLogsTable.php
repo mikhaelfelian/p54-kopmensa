98f7575346fd1fac60591673ae689d6ed8c291a2
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-10-27 12:07:21 +0700

    Create 2025_09_17_033642_CreateUserLogsTable.php
    Added userLog table

---
+++ app/Database/Migrations/2025_09_17_033642_CreateUserLogsTable.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUserLogsTable extends Migration
+{
+    public function up()
+    {
+        // Drop table if exists
+        $this->forge->dropTable('user_logs', true);
+        
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'user_id' => [
+                'type'       => 'INT',
+                'constraint' => 11,
+                'unsigned'   => true,
+            ],
+            'action' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+            ],
+            'description' => [
+                'type' => 'TEXT',
+            ],
+            'metadata' => [
+                'type' => 'JSON',
+                'null' => true,
+            ],
+            'ip_address' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 45,
+            ],
+            'user_agent' => [
+                'type' => 'TEXT',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => false,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('user_id');
+        $this->forge->addKey('action');
+        $this->forge->createTable('user_logs');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('user_logs');
+    }
+}
+
