9a502011e7774a70ef79f00d7d627e9c4d49f2a2
Mikhael Felian Waskito <mikhaelfelian@gmail.com>
2025-10-28 21:39:34 +0700

    Create AddPpnAmountToTblTransBeliDet.php

---
+++ app/Database/Migrations/AddPpnAmountToTblTransBeliDet.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class AddPpnAmountToTblTransBeliDet extends Migration
+{
+    public function up()
+    {
+        $db = \Config\Database::connect();
+        $query = $db->query("SHOW COLUMNS FROM tbl_trans_beli_det LIKE 'ppn_amount'");
+        
+        if ($query->getNumRows() === 0) {
+            $fields = [
+                'ppn_amount' => [
+                    'type'       => 'DECIMAL',
+                    'constraint' => '12,2',
+                    'default'    => 0,
+                    'null'       => false,
+                    'comment'    => 'PPN amount for this line item',
+                    'after'      => 'subtotal'
+                ],
+            ];
+            $this->forge->addColumn('tbl_trans_beli_det', $fields);
+        }
+    }
+
+    public function down()
+    {
+        $db = \Config\Database::connect();
+        $query = $db->query("SHOW COLUMNS FROM tbl_trans_beli_det LIKE 'ppn_amount'");
+        
+        if ($query->getNumRows() > 0) {
+            $this->forge->dropColumn('tbl_trans_beli_det', 'ppn_amount');
+        }
+    }
+}
+
